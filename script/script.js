61c4b5d188159a149e340bbd0473e10177067394
Coriolan <andriamiharinjaracoriolanr.acr@gmail.com>
2024-11-11 00:06:03 +0300

    Basic navigation and styling

---
+++ script/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    const location = this.location.href.split('/')[3];
+    //console.log(location);
+    //document.head.appendChild('')
+    const header = this.createElement('div');
+    const main = this.createElement('div');
+    const footer = this.createElement('div');
+    
+    header.classList.add('header');
+    main.classList.add('main');
+    footer.classList.add('footer');
+    footer.innerHTML = main.innerHTML = "<h1>Essai</h1>";
+
+    if(location=="index.html"){
+        header.innerHTML = "\
+        <div><h1>Riasec Project</h1></div>\
+        <div>\
+            <div class=\"link\"><a href=\"login.html\">Se connecter</a></div>\
+        </div>\
+        ";
+    }
+    else if(location=="login.html"){
+        header.innerHTML = "\
+        <div><h1>Riasec Project</h1></div>\
+        <div>\
+            <div class=\"link\"><a href=\"index.html\">Revenir à l'accueil</a></div>\
+        </div>\
+        ";
+    }
+    else{
+        header.innerHTML = "\
+        <div><h1>Riasec Project</h1></div>\
+        <div>\
+            <div class=\"link\"><a href=\"riasec.html\">Accueil</a></div>\
+            <div class=\"link\"><a href=\"test.html\">Passer le test</a></div>\
+            <div class=\"link\"><a href=\"nous.html\">Nous connaître</a></div>\
+        </div>\
+        ";
+    }
+
+    document.body.appendChild(header);
+    this.body.appendChild(main);
+    this.body.appendChild(footer);
+})
