3a928166df9545b78967a954e2b637b48cff4c94
Coriolan <andriamiharinjaracoriolanr.acr@gmail.com>
2024-11-29 21:34:35 +0300

    Amélioration de la gestion de l'interface de test

--- script/test-pagination.js
+++ script/test-pagination.js
@@ -1,34 +1,49 @@
 document.addEventListener("DOMContentLoaded",function(){
     const classes = document.getElementsByClassName('class');
     const submit = document.getElementById('submit-test');
+    var index = 0;
     const navBtn = {
         prev:document.getElementById('prev'),
-        next:document.getElementById('next')
+        next:document.getElementById('next'),
     }
     
-    //console.log(classes);
-    var number = 0;
+    const number = classes.length;
+    console.log(number);
+
     navBtn.prev.addEventListener("click",function(){
-        number--;
-        number+=5;
-        number%=5;
+        if(index==5){
+            index=1;
+        }
+        else{
+            index--;
+        }
+        index+=number;
+        index%=number;
         actual();
     })
     navBtn.next.addEventListener("click",function(){
-        number++;
-        number%=5;
+        index++;
+        index%=number;
         actual();
     })
 
+    submit.addEventListener("click",function(){
+        index++;
+        index%=number;
+        actual();
+    })
+    
     actual();
 
     function actual(){
         for(let i=0;i<Object.keys(classes).length;i++){
-            classes[i].style.display= i!=number?"none":"";
-            prev.style.display = i==0?"none":"";
-            next.style.display = i==3?"none":"";
-            //submit.style.display = i!=3?"none":"";
+            classes[i].style.display= i!=index?"none":"";    
         }
+        navBtn.next.value = index==0?"Commencer":"Prochain";
+        navBtn.prev.value = index==5?"Revisionner les réponses":"Précédent";
+        navBtn.prev.style.display = index<=1?"none":"";
+        navBtn.next.style.display = index>=4?"none":"";
+        submit.style.display = index!=4?"none":"";
     }
     
 })
--- script/test.js
+++ script/test.js
@@ -1,157 +1,72 @@
 document.addEventListener("DOMContentLoaded",function(){
-    var viar,viai,viaa,vias,viae,viac,vior,vioi,vioa,vios,vioe,vioc,vapr,vapi,vapa,vaps,vape,vapc,vper,vpei,vpea,vpes,vpee,vpec;
+    const values = new Array(24).fill(0);
+    //console.log(values);
 
-    const iar = this.getElementsByClassName('iar');
-    const iarButton = this.getElementById('iar');
-    const iai = this.getElementsByClassName('iai');
-    const iaiButton = this.getElementById('iai');
-    const iaa = this.getElementsByClassName('iaa');
-    const iaaButton = this.getElementById('iaa');
-    const ias = this.getElementsByClassName('ias');
-    const iasButton = this.getElementById('ias');
-    const iae = this.getElementsByClassName('iae');
-    const iaeButton = this.getElementById('iae');
-    const iac = this.getElementsByClassName('iac');
-    const iacButton = this.getElementById('iac');
-    const ior = this.getElementsByClassName('ior');
-    const iorButton = this.getElementById('ior');
-    const ioi = this.getElementsByClassName('ioi');
-    const ioiButton = this.getElementById('ioi');
-    const ioa = this.getElementsByClassName('ioa');
-    const ioaButton = this.getElementById('ioa');
-    const ios = this.getElementsByClassName('ios');
-    const iosButton = this.getElementById('ios');
-    const ioe = this.getElementsByClassName('ioe');
-    const ioeButton = this.getElementById('ioe');
-    const ioc = this.getElementsByClassName('ioc');
-    const iocButton = this.getElementById('ioc');
-    const apr = this.getElementsByClassName('ar');
-    const aprButton = this.getElementById('ar');
-    const api = this.getElementsByClassName('ai');
-    const apiButton = this.getElementById('ai');
-    const apa = this.getElementsByClassName('aa');
-    const apaButton = this.getElementById('aa');
-    const aps = this.getElementsByClassName('as');
-    const apsButton = this.getElementById('as');
-    const ape = this.getElementsByClassName('ae');
-    const apeButton = this.getElementById('ae');
-    const apc = this.getElementsByClassName('ac');
-    const apcButton = this.getElementById('ac');
-    const per = this.getElementsByClassName('pr');
-    const perButton = this.getElementById('pr');
-    const pei = this.getElementsByClassName('pi');
-    const peiButton = this.getElementById('pi');
-    const pea = this.getElementsByClassName('pa');
-    const peaButton = this.getElementById('pa');
-    const pes = this.getElementsByClassName('ps');
-    const pesButton = this.getElementById('ps');
-    const pee = this.getElementsByClassName('pe');
-    const peeButton = this.getElementById('pe');
-    const pec = this.getElementsByClassName('pc');
-    const pecButton = this.getElementById('pc');
+    const modules = {
+        0:document.getElementsByClassName('iar'),
+        1:document.getElementsByClassName('iai'),
+        2:document.getElementsByClassName('iaa'),
+        3:document.getElementsByClassName('ias'),
+        4:document.getElementsByClassName('iae'),
+        5:document.getElementsByClassName('iac'),
+        6:document.getElementsByClassName('ior'),
+        7:document.getElementsByClassName('ioi'),
+        8:document.getElementsByClassName('ioa'),
+        9:document.getElementsByClassName('ios'),
+        10:document.getElementsByClassName('ioe'),
+        11:document.getElementsByClassName('ioc'),
+        12:document.getElementsByClassName('ar'),
+        13:document.getElementsByClassName('ai'),
+        14:document.getElementsByClassName('aa'),
+        15:document.getElementsByClassName('as'),
+        16:document.getElementsByClassName('ae'),
+        17:document.getElementsByClassName('ac'),
+        18:document.getElementsByClassName('pr'),
+        19:document.getElementsByClassName('pi'),
+        20:document.getElementsByClassName('pa'),
+        21:document.getElementsByClassName('ps'),
+        22:document.getElementsByClassName('pe'),
+        23:document.getElementsByClassName('pc'),
+    }
     
     const submit = this.getElementById('submit-test');
-    const result = this.getElementById('result');
     const cells = this.getElementById('result-table').getElementsByTagName('td');
     
-    
-    //console.log(cells);
-    eventListener(iarButton,iar,viar);
-    eventListener(iaiButton,iai,viai);
-    eventListener(iaaButton,iaa,viaa);
-    eventListener(iasButton,ias,vias);
-    eventListener(iaeButton,iae,viae);
-    eventListener(iacButton,iac,viac);
-    eventListener(iorButton,ior,vior);
-    eventListener(ioiButton,ioi,vioi);
-    eventListener(ioaButton,ioa,vioa);
-    eventListener(iosButton,ios,vios);
-    eventListener(ioeButton,ioe,vioe);
-    eventListener(iocButton,ioc,vioc);
-    eventListener(aprButton,apr,vapr);
-    eventListener(apiButton,api,vapi);
-    eventListener(apaButton,apa,vapa);
-    eventListener(apsButton,aps,vaps);
-    eventListener(apeButton,ape,vape);
-    eventListener(apcButton,apc,vapc);
-    eventListener(perButton,per,vper);
-    eventListener(peiButton,pei,vpei);
-    eventListener(peaButton,pea,vpea);
-    eventListener(pesButton,pes,vpes);
-    eventListener(peeButton,pee,vpee);
-    eventListener(pecButton,pec,vpec);
 
     submit.addEventListener("click",function(){
-        countChecked(iarButton,iar,viar);
-        countChecked(iaiButton,iai,viai);
-        countChecked(iaaButton,iaa,viaa);
-        countChecked(iasButton,ias,vias);
-        countChecked(iaeButton,iae,viae);
-        countChecked(iacButton,iac,viac);
-        countChecked(iorButton,ior,vior);
-        countChecked(ioiButton,ioi,vioi);
-        countChecked(ioaButton,ioa,vioa);
-        countChecked(iosButton,ios,vios);
-        countChecked(ioeButton,ioe,vioe);
-        countChecked(iocButton,ioc,vioc);
-        countChecked(aprButton,apr,vapr);
-        countChecked(apiButton,api,vapi);
-        countChecked(apaButton,apa,vapa);
-        countChecked(apsButton,aps,vaps);
-        countChecked(apeButton,ape,vape);
-        countChecked(apcButton,apc,vapc);
-        countChecked(perButton,per,vper);
-        countChecked(peiButton,pei,vpei);
-        countChecked(peaButton,pea,vpea);
-        countChecked(pesButton,pes,vpes);
-        countChecked(peeButton,pee,vpee);
-        countChecked(pecButton,pec,vpec);
-
-        cells[0].innerHTML=iarButton.value;
-        cells[1].innerHTML=iaiButton.value;
-        cells[2].innerHTML=iaaButton.value;
-        cells[3].innerHTML=iasButton.value;
-        cells[4].innerHTML=iaeButton.value;
-        cells[5].innerHTML=iacButton.value;
-        cells[6].innerHTML=iorButton.value;
-        cells[7].innerHTML=ioiButton.value;
-        cells[8].innerHTML=ioaButton.value;
-        cells[9].innerHTML=iosButton.value;
-        cells[10].innerHTML=ioeButton.value;
-        cells[11].innerHTML=iocButton.value;
-        cells[12].innerHTML=aprButton.value;
-        cells[13].innerHTML=apiButton.value;
-        cells[14].innerHTML=apaButton.value;
-        cells[15].innerHTML=apsButton.value;
-        cells[16].innerHTML=apeButton.value;
-        cells[17].innerHTML=apcButton.value;
-        cells[18].innerHTML=perButton.value;
-        cells[19].innerHTML=peiButton.value;
-        cells[20].innerHTML=peaButton.value;
-        cells[21].innerHTML=pesButton.value;
-        cells[22].innerHTML=peeButton.value;
-        cells[23].innerHTML=pecButton.value;
-        cells[24].innerHTML=parseInt(cells[0].innerHTML)+parseInt(cells[6].innerHTML)+parseInt(cells[12].innerHTML)+parseInt(cells[18].innerHTML);
-        cells[25].innerHTML=parseInt(cells[1].innerHTML)+parseInt(cells[7].innerHTML)+parseInt(cells[13].innerHTML)+parseInt(cells[19].innerHTML);
-        cells[26].innerHTML=parseInt(cells[2].innerHTML)+parseInt(cells[8].innerHTML)+parseInt(cells[14].innerHTML)+parseInt(cells[20].innerHTML);
-        cells[27].innerHTML=parseInt(cells[3].innerHTML)+parseInt(cells[9].innerHTML)+parseInt(cells[15].innerHTML)+parseInt(cells[21].innerHTML);
-        cells[28].innerHTML=parseInt(cells[4].innerHTML)+parseInt(cells[10].innerHTML)+parseInt(cells[16].innerHTML)+parseInt(cells[22].innerHTML);
-        cells[29].innerHTML=parseInt(cells[5].innerHTML)+parseInt(cells[11].innerHTML)+parseInt(cells[17].innerHTML)+parseInt(cells[23].innerHTML);
+        countChecked(cells,modules,values);
+        /*        
+        if(number==0){
+            cells[24].innerHTML=parseInt(cells[0].innerHTML)+parseInt(cells[6].innerHTML)+parseInt(cells[12].innerHTML)+parseInt(cells[18].innerHTML);
+            cells[25].innerHTML=parseInt(cells[1].innerHTML)+parseInt(cells[7].innerHTML)+parseInt(cells[13].innerHTML)+parseInt(cells[19].innerHTML);
+            cells[26].innerHTML=parseInt(cells[2].innerHTML)+parseInt(cells[8].innerHTML)+parseInt(cells[14].innerHTML)+parseInt(cells[20].innerHTML);
+            cells[27].innerHTML=parseInt(cells[3].innerHTML)+parseInt(cells[9].innerHTML)+parseInt(cells[15].innerHTML)+parseInt(cells[21].innerHTML);
+            cells[28].innerHTML=parseInt(cells[4].innerHTML)+parseInt(cells[10].innerHTML)+parseInt(cells[16].innerHTML)+parseInt(cells[22].innerHTML);
+            cells[29].innerHTML=parseInt(cells[5].innerHTML)+parseInt(cells[11].innerHTML)+parseInt(cells[17].innerHTML)+parseInt(cells[23].innerHTML);
+        }*/
     })
 })
 
-async function countChecked(button,list,value){
-    value = 0;
-    for(let i=0;i<list.length;i++){
-        value+=list[i].type=="checkbox"?list[i].checked:parseInt(list[i].value);
+async function countChecked(cells,modules,values){
+    for(let i=0;i<Object.keys(modules).length;i++){
+        //console.log(i);
+        values[i]=0;
+        //console.log(modules[i].length);
+        for(let j=0;j<modules[i].length;j++){
+            values[i]+=modules[i][j].type=="checkbox"?modules[i][j].checked:parseInt(modules[i][j].value);
+        }
+        
+        if(isNaN(values[i])){
+            values[i]=0;
+            alert("Certaines entrées ne sont pas correctes pour les aptitudes.");
+            return(1);
+        }
     }
-    button.value=value;
-    //console.log(value);
-}
-async function eventListener(button,list,value){
-    //console.log(list);
-    countChecked(button,list,value);
-    button.addEventListener("click",function(){
-        countChecked(button,list,value);
-    });
+    for(let i=0;i<Object.keys(modules).length;i++){
+        cells[i].innerHTML=values[i];
+    }
+    for(let i=0;i<6;i++){
+        cells[24+i].innerHTML=values[i]+values[6+i]+values[12+i]+values[18+i];
+    }
+    return 0;
 }
